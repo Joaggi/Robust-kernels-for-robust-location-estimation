3446c1fed16c83f97c02d388d5b5e22f78b7d34e
joaggi <joaggi@gmail.com>
2014-12-10 21:47:20 -0500

    modified:   kernelconvexnmf_experimentATT.m 	modified:   run.txt

--- Occlusion/ATT/Kernelconvexnmf/kernelconvexnmf_experimentATT.m
+++ Occlusion/ATT/Kernelconvexnmf/kernelconvexnmf_experimentATT.m
@@ -1,4 +1,4 @@
 addpath '../../../Algorithms/Matlab';
-addpath '../../../Occlussion/Experiments/Kernelconvexnmf';
+addpath '../../../Occlusion/Experiments/Kernelconvexnmf';
 vect = [6,7,8,9,10,11,12,13]
-kernelconvexnmf_experiment('../../../Occlussion/ATT','att_occlusion_contamination','att_occlusion_contamination',vect,strcat('Experimento ',date))
+kernelconvexnmf_experiment('../../../Occlusion/ATT','ATT_occlusion_contamination','ATT_occlusion_contamination',vect,strcat('Experimento ',date,' 2'))
