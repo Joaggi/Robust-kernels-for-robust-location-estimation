7b5119bc05fb9fe705a59d3056f0296af4445588
joaggi <joaggi@gmail.com>
2015-01-27 21:05:41 -0500

    Modified gaussian_cnmf
    Modified

--- learning_linear_and_rbf/python/att/gaussian_cnmf/gaussian_cnmf.py
+++ learning_linear_and_rbf/python/att/gaussian_cnmf/gaussian_cnmf.py
@@ -8,6 +8,8 @@ from semantic_methods_toolkit.python.tunning_parameter_unsupervised_technique \
     import tunning_parameter_unsupervised_technique
 from semantic_methods_toolkit.python.utils.generate_logarithm_vector_kernel \
     import generate_logarithm_vector_kernel
+from semantic_methods_toolkit.python.preprocess_data.normalize_by_range \
+    import normalize_by_range
 
 
 data, labels = dataset_factory.dataset_factory('../../../../dataset/att.mat',
@@ -20,18 +22,20 @@ k = np.unique(labels).size
 
 #data = normalize_by_range(data, axis=0)
 vect_to_prove = [2**x for x in np.arange(-20, 20)]
-vect = generate_logarithm_vector_kernel(data, vect_to_prove, percentage=0.5)
+vector = generate_logarithm_vector_kernel(data, vect_to_prove, percentage=0.5)
 
-dt = [('key', 'S30'), ('value', 'S30')]
+vector = [2**8]
+
+dt = [('key', 'S100'), ('value', 'S100')]
 arr = np.zeros((10,), dtype=dt)
 
 arr[0]['value'] = str(k)
 arr[0]['key'] = 'k'
-arr[1]['value'] = "iter: " + str(3000)
+arr[1]['value'] = "iter: " + str(1000)
 arr[1]['key'] = 'termination_criterion'
 arr[2]['value'] = 'random'
 arr[2]['key'] = 'initialization'
-arr[3]['value'] = '10'
+arr[3]['value'] = '30'
 arr[3]['key'] = 'epocs'
 arr[4]['value'] = '1'
 arr[4]['key'] = 'clustering_accuracy'
@@ -41,7 +45,7 @@ arr[6]['value'] = 'rbf'
 arr[6]['key'] = 'kernel'
 arr[7]['value'] = '1'
 arr[7]['key'] = 'param'
-arr[8]['value'] = str(vect)
+arr[8]['value'] = str(vector)
 arr[8]['key'] = 'vect'
 
 
@@ -55,10 +59,10 @@ def options(vect):
               arr[5]['key']: int(arr[5]['value']),
               arr[6]['key']: arr[6]['value'],
               arr[7]['key']: int(arr[7]['value']),
-              arr[8]['key']:  vect}
+              arr[8]['key']:  vector}
     for i in vect:
         #define sigma
-        option['param'] = 2**i
+        option['param'] = i
         yield option
 
 option = {arr[0]['key']: int(arr[0]['value']),
@@ -69,12 +73,12 @@ option = {arr[0]['key']: int(arr[0]['value']),
           arr[5]['key']: int(arr[5]['value']),
           arr[6]['key']: arr[6]['value'],
           arr[7]['key']: int(arr[7]['value']),
-          arr[8]['key']: vect}
+          arr[8]['key']: vector}
 
 cnmf_experiment = CnmfExperiment(data, option)
 
 options_results_performance, results_performance = tunning_parameter_unsupervised_technique\
-    (data, labels, cnmf_experiment, options(vect))
+    (data, labels, cnmf_experiment, options(vector))
 
 import time
 ## dd/mm/yyyy format
