5fe6cac7188b09e9401f5ac090c495dd028315b4
joaggi <joaggi@gmail.com>
2015-02-07 18:20:00 -0500

    Fix

---
+++ matlab/+learning_linear_and_rbf/+jaffe/+gaussian_kernel_cnmf/jaffe_gaussian_kernel_cnmf.m
@@ -0,0 +1,21 @@
+import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
+
+options.vect = 2.^(1);
+options.dataset.name= 'att'
+options.dataset.dataset= 'data'
+options.dataset.labels= 'labels'
+
+options.epocs = 20
+
+options.algorithm.kernel = 'rbf'
+options.algorithm.iter=3000;
+options.algorithm.dis=1;
+options.algorithm.residual=1e-70;
+options.algorithm.tof=1e-70;
+options.algorithm.initialization='random';
+options.algorithm.random = 1
+options.algorithm.name = 'kernel_convex_nmf'
+
+options.preprocessing = 'normalize_by_range'
+
+learning_gaussian_kernel_cnmf(options)
