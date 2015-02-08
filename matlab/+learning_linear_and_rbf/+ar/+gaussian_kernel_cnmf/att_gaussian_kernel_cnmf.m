468156b030f01a8f53af65bb881e3b2931899693
joaggi <joaggi@gmail.com>
2015-02-08 15:25:16 -0500

    Matlab fix
    matlab learning

--- matlab/+learning_linear_and_rbf/+ar/+gaussian_kernel_cnmf/att_gaussian_kernel_cnmf.m
+++ matlab/+learning_linear_and_rbf/+ar/+gaussian_kernel_cnmf/att_gaussian_kernel_cnmf.m
@@ -1,13 +1,13 @@
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 options.vect = 2.^(0);
-options.dataset.name= 'att'
+options.dataset.name= 'ar'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
@@ -15,6 +15,6 @@ options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 2
 
-options.preprocessing = 'normalize_by_range'
+options.preprocessing = 'raw'
 
 learning_gaussian_kernel_cnmf(options)
--- matlab/+learning_linear_and_rbf/+results/learning_kmeans.m
+++ matlab/+learning_linear_and_rbf/+results/learning_kmeans.m
@@ -5,21 +5,49 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
+options.dataset.name= 'att'
+options.dataset.dataset= 'data'
+options.dataset.labels= 'labels'
+
+options.epocs = 20
+
+options.algorithm.kernel = 'linear'
+options.algorithm.iter=3000;
+options.algorithm.dis=1;
+options.algorithm.residual=1e-70;
+options.algorithm.tof=1e-70;
+options.algorithm.initialization='random';
+options.algorithm.random = 1
+options.algorithm.name = 'kmeans'
+
+options.preprocessing = 'normalize_by_range'
+
+learning_gaussian_kernel_cnmf(options)
+
+
+addpath('/home/jagallegom/Machine-Learning/algorithms/matlab/')
+addpath('/home/jagallegom/Machine-Learning/dataset/')
+addpath('/home/jagallegom/Machine-Learning/matlab/')
+
+import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
+
+
+options.vect = 2.^(0);
 options.dataset.name= 'balance_scale'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
@@ -32,21 +60,21 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
 options.dataset.name= 'glass'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
@@ -59,21 +87,21 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
 options.dataset.name= 'jaffe'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
@@ -86,21 +114,21 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
 options.dataset.name= 'movement_libras'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
@@ -113,21 +141,21 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
 options.dataset.name= 'wineq'
 options.dataset.dataset= 'data'
 options.dataset.labels= 'labels'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
@@ -140,21 +168,49 @@ addpath('/home/jagallegom/Machine-Learning/matlab/')
 import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
 
 
-options.vect = 2.^(1);
+options.vect = 2.^(0);
 options.dataset.name= 'mnist_2k'
 options.dataset.dataset= 'data_train_2000'
 options.dataset.labels= 'labels_train_2000'
 
 options.epocs = 20
 
-options.algorithm.kernel = 'rbf'
+options.algorithm.kernel = 'linear'
 options.algorithm.iter=3000;
 options.algorithm.dis=1;
 options.algorithm.residual=1e-70;
 options.algorithm.tof=1e-70;
 options.algorithm.initialization='random';
 options.algorithm.random = 1
-options.algorithm.name = 'kernel_convex_nmf'
+options.algorithm.name = 'kmeans'
+
+options.preprocessing = 'normalize_by_range'
+
+learning_gaussian_kernel_cnmf(options)
+
+
+addpath('/home/jagallegom/Machine-Learning/algorithms/matlab/')
+addpath('/home/jagallegom/Machine-Learning/dataset/')
+addpath('/home/jagallegom/Machine-Learning/matlab/')
+
+import learning_linear_and_rbf.experiments.gaussian_kernel_cnmf.learning_gaussian_kernel_cnmf
+
+
+options.vect = 2.^(0);
+options.dataset.name= 'abalone'
+options.dataset.dataset= 'data'
+options.dataset.labels= 'labels'
+
+options.epocs = 20
+
+options.algorithm.kernel = 'linear'
+options.algorithm.iter=3000;
+options.algorithm.dis=1;
+options.algorithm.residual=1e-70;
+options.algorithm.tof=1e-70;
+options.algorithm.initialization='random';
+options.algorithm.random = 1
+options.algorithm.name = 'kmeans'
 
 options.preprocessing = 'normalize_by_range'
 
